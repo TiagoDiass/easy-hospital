451065d59179b60627c5261ce038dea7d268eea4
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-03 09:33:43 -0300

    migration to create patients and seed made

--- backend/src/database/migrations/20200802164250_create_table_patients.js
+++ backend/src/database/migrations/20200802164250_create_table_patients.js
@@ -2,6 +2,14 @@ exports.up = function (knex) {
   return knex.schema.createTable('patients', (table) => {
     table.increments('id').primary();
     table.string('name').notNull();
+    table.string('email').notNull().unique();
+    table.string('phone').notNull().unique();
+    table.date('birth').notNull();
+    table.string('gender').notNull();
+    table.float('weight').notNull();
+    table.integer('height').notNull();
+    table.string('blood_type').notNull();
+    table.timestamps();
   });
 };
 
---
+++ backend/src/database/seeds/001_patients.js
@@ -0,0 +1,40 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('patients')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('patients').insert([
+        {
+          name: 'Tiago Dias',
+          email: 'tiago@gmail.com',
+          phone: '+5519912341234',
+          birth: '2001-12-12',
+          gender: 'M',
+          weight: 55.6,
+          height: 175,
+          blood_type: 'A+',
+        },
+        {
+          name: 'Maria Paula',
+          email: 'mariapaula@gmail.com',
+          phone: '+5519990909090',
+          birth: '2008-06-15',
+          gender: 'F',
+          weight: 37.6,
+          height: 165,
+          blood_type: 'B+',
+        },
+        {
+          name: 'Lucas Lima',
+          email: 'luquinhas@gmail.com',
+          phone: '+551965656565',
+          birth: '2003-08-27',
+          gender: 'M',
+          weight: 50.3,
+          height: 169,
+          blood_type: 'A-',
+        },
+      ]);
+    });
+};

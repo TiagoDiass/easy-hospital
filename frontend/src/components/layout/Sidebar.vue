f7ce9f1cb9dd783f0969029e49d41a784df77c5d
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-05 21:48:44 -0300

    sidebar made

--- frontend/src/components/layout/Sidebar.vue
+++ frontend/src/components/layout/Sidebar.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="sidebar">
     <h1 class="logo text-300"><span class="text-bold text-secondary">Easy</span>Hospital</h1>
+
+    <div id="nav">
+      <a href="#"><i class="fas fa-home"></i> Home</a>
+      <a href="#"><i class="fas fa-home"></i> Home</a>
+      <a href="#"><i class="fas fa-home"></i> Home</a>
+      <a href="#"><i class="fas fa-home"></i> Home</a>
+      <a href="#"><i class="fas fa-home"></i> Home</a>
+    </div>
   </div>
 </template>
 
